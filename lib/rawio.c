6d3996dc014eca0d1e69a48f9880ac13dc059153
Don J. Rude <rudedude@gmail.com>
2025-05-25 21:43:42 -0400

    auogenerated have files

--- lib/rawio.c
+++ lib/rawio.c
@@ -70,10 +70,9 @@ double lava_dur_arg = 0.0;	/* duration requested for alarm */
  */
 static int isalarm = 0;	/* 1 => alarm has been set */
 static int issimple = 0;	/* 1 => simple alarm has been set */
-struct itimerval invalid_timer;	/* not a valid alarm time */
-invalid_timer.it_value.tv_sec = -1;
-invalid_timer.it_value.tv_usec = -1;
-
+static struct itimerval invalid_timer = {	/* not a valid alarm time */
+    {-1, -1}, {-1, -1}
+};
 struct itimerval timer = {	/* current alarm time */
     {0, 0}, {0, 0}
 };
